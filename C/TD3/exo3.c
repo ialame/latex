55bd603c0671af575ca0f217e5713a2d6b07d789
ibrahim <ibrahim.alame@gmail.com>
2023-12-06 07:43:11 +0100

    TP1 tex

--- C/TD3/exo3.c
+++ C/TD3/exo3.c
@@ -1,15 +1,22 @@
-#include <stdio.h> 
-void miroir() { 
-    char s[20],c; 
-    int i=0; 
-    while( (c=getchar())!='?') 
-        s[i++]=c; 
-    s[i]='\0'; 
-    while (--i>=0) 
-        putchar (s[i]); 
-}
-void main() { 
-    printf("Entrer les caracteres avec ? a la fin.\n"); 
-    miroir(); 
-    
+#include <stdio.h>
+#include <stdlib.h>
+#define N 3
+#define M 5
+int main(){
+    int i;
+    char** A=NULL;
+    if((A=malloc(N))!=NULL){
+        for(i=0;i<N;i++){
+            A[i]=(char*)malloc(M);
+            printf("Entrer le mot %d: ");
+            scanf("%s",A[i]);
+        }
+    }else{
+        printf("malloc failed");
+        exit(EXIT_FAILLURE);
+    }
+    for(i=0;i<N;i++)
+        printf("Mot %d = %s ",i,A[i]);
+    free(A);
+    return 0;
 }
